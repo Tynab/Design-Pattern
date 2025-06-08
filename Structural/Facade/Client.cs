80c582a65d6da93279c91a2b18b9cbb90d04aae8
Yami An <yamiannephilim@gmail.com>
2025-06-08 22:40:40 +0700

    facade

---
+++ Structural/Facade/Client.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+namespace Facade;
+
+internal sealed class Client
+{
+    internal static void ClientCode(Facade facade) => WriteLine(facade.Operation());
+}
---
+++ Structural/Facade/Program.cs
@@ -0,0 +1,16 @@
+﻿using Facade;
+
+var subsystem1 = new Subsystem1();
+var subsystem2 = new Subsystem2();
+var facade = new Facade.Facade(subsystem1, subsystem2);
+
+Client.ClientCode(facade);
+
+/*
+ * Facade initializes subsystems:
+ * Subsystem1: Ready!
+ * Subsystem2: Get ready!
+ * Facade orders subsystems to perform the action:
+ * Subsystem1: Go!
+ * Subsystem2: Fire!
+ */
